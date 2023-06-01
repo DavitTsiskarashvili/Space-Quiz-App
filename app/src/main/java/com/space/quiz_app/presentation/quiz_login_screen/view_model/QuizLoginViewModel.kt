e9dc7cf9f0eb1b35e67f78c390ed060bddf5a4e3
DavitTsiskarashvili <datotsiskarashvili@gmail.com>
2023-06-01 16:21:55 +0400

    added username null checking

--- app/src/main/java/com/space/quiz_app/presentation/quiz_login_screen/view_model/QuizLoginViewModel.kt
+++ app/src/main/java/com/space/quiz_app/presentation/quiz_login_screen/view_model/QuizLoginViewModel.kt
@@ -3,16 +3,19 @@ package com.space.quiz_app.presentation.quiz_login_screen.view_model
 import com.space.quiz_app.common.extensions.viewModelScope
 import com.space.quiz_app.domain.repository.QuizUserRepository
 import com.space.quiz_app.presentation.base.QuizBaseViewModel
+import com.space.quiz_app.presentation.mapper.QuizUserDomainToUIMapper
 import com.space.quiz_app.presentation.mapper.QuizUserUIToDomainMapper
 import com.space.quiz_app.presentation.model.QuizUserUIModel
 import com.space.quiz_app.presentation.quiz_login_screen.ui.QuizLoginFragmentDirections
 import com.space.quiz_app.presentation.utils.QuizUsernameValidation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 
 class QuizLoginViewModel(
     private val quizUserRepository: QuizUserRepository,
-    private val quizUserUIToDomainMapper: QuizUserUIToDomainMapper
+    private val quizUserUIToDomainMapper: QuizUserUIToDomainMapper,
+    private val quizUserDomainToUIMapper: QuizUserDomainToUIMapper
 ) : QuizBaseViewModel() {
 
     private val _validationError = MutableStateFlow<QuizUsernameValidation?>(null)
@@ -26,6 +29,18 @@ class QuizLoginViewModel(
                 navigate()
             } else {
                 _validationError.emit(validity)
+            }
+        }
+    }
+
+    fun checkUserLogState(username: String){
+        viewModelScope {
+            quizUserRepository.getEntityIfLoggedIn().collect{
+                if (it == null){
+                    insertUsername(QuizUserUIModel(username))
+                } else {
+                    insertUsername(quizUserDomainToUIMapper(it.copy(isLoggedIn = true)))
+                }
             }
         }
     }
